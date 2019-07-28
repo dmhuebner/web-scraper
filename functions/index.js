758fe1546eb42ff6929891dc675b8d04c3eea0cc
David <davidmh.mail@gmail.com>
2019-07-28 15:08:47 -0600

    Implement Instagram image scraper with puppeteer

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const cors = require('cors')({origin: true}),
     cheerio = require('cheerio'),
     getUrls = require('get-urls'),
-    fetch = require('node-fetch');
+    fetch = require('node-fetch'),
+    puppeteer = require('puppeteer');
 
 const scrapeMetatags = (text) => {
     const urls = Array.from(getUrls(text));
@@ -31,9 +32,45 @@ const scrapeMetatags = (text) => {
     });
 };
 
+const scrapeImages = async (loginUrl, username, password) => {
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+
+    await page.goto(loginUrl);
+
+    await page.screenshot({path: '1.png'});
+
+    await page.type('[name=username]', username);
+    await page.type('[name=password]', password);
+
+    await page.screenshot({path: '2.png'});
+
+    await page.click('[type=submit]');
+
+    await page.waitFor(5000);
+
+    await page.goto(`https://www.instagram.com/${username}`);
+
+    await page.waitForSelector('img', {visible: true});
+
+    await page.screenshot({path: '3.png'});
+
+    // Execute code in the DOM - scrape
+
+    const data = await page.evaluate(() => {
+        const images = document.querySelectorAll('img');
+
+        const urls = Array.from(images).map(image => image.src);
+
+        return urls;
+    });
+
+    await browser.close();
+};
+
 // Test
 const execute = async () => {
-    const result = await scrapeMetatags('https://nytimes.com/');
+    const result = await scrapeMetatags('https://twitter.com/');
     console.log(result);
 };
 
